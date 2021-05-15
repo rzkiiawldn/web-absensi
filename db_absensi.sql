f1b39e377772cc4156683db35741daaaa8916145
rzkiiawldn <rizkiawaludin323@gmail.com>
2021-05-15 15:30:48 +0700

    menambahkan db

---
+++ db_absensi.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Bulan Mei 2021 pada 10.29
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.3.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_absensi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` time NOT NULL,
+  `keterangan` enum('Masuk','Pulang','','','') NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `tanggal`, `waktu`, `keterangan`, `id_user`) VALUES
+(23, '2021-05-14', '23:47:38', 'Masuk', 3),
+(24, '2021-05-14', '23:54:02', 'Pulang', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jam_kerja`
+--
+
+CREATE TABLE `jam_kerja` (
+  `id_jam` int(11) NOT NULL,
+  `mulai` time NOT NULL,
+  `selesai` time NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jam_kerja`
+--
+
+INSERT INTO `jam_kerja` (`id_jam`, `mulai`, `selesai`, `keterangan`) VALUES
+(1, '08:00:00', '09:00:00', 'Masuk'),
+(2, '16:00:00', '17:00:00', 'Pulang');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(225) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `nik`, `password`, `id_level`) VALUES
+(1, 'Rizki Awaludin', 'IT-0001', '$2y$10$yRVVO0jgX2skJVTNEnEGh.Hzj0q7HQuhtlW4DLIdvlscOkhwI./va', 1),
+(2, 'HRD', 'IT-0002', '$2y$10$7n4WNLOu/TVFEi3vu/R/fOVEpe0QxD07A2ALwC/2FltSoE/CZgmKO', 2),
+(3, 'Pegawai', 'IT-0003', '$2y$10$vcunJckg4W11aQ/YNk/GvuFdiRXRuMcvraGCk4CfvLiMcAu8EBGc6', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_akses_menu`
+--
+
+CREATE TABLE `user_akses_menu` (
+  `id_akses` int(11) NOT NULL,
+  `id_level` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `id_sub` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_akses_menu`
+--
+
+INSERT INTO `user_akses_menu` (`id_akses`, `id_level`, `id_menu`, `id_sub`) VALUES
+(1, 1, 1, 1),
+(2, 1, 2, 2),
+(3, 1, 2, 3),
+(4, 1, 2, 6),
+(5, 1, 1, 4),
+(6, 1, 1, 5),
+(9, 2, 2, 2),
+(10, 2, 2, 11),
+(11, 1, 2, 11),
+(12, 3, 2, 2),
+(15, 2, 4, 7),
+(16, 2, 4, 9),
+(17, 3, 4, 7),
+(18, 3, 4, 8),
+(19, 3, 4, 10),
+(20, 3, 2, 11),
+(21, 1, 4, 0),
+(22, 1, 4, 7),
+(23, 1, 4, 9),
+(24, 2, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_level`
+--
+
+CREATE TABLE `user_level` (
+  `id_level` int(11) NOT NULL,
+  `level` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_level`
+--
+
+INSERT INTO `user_level` (`id_level`, `level`) VALUES
+(1, 'Admin'),
+(2, 'HRD'),
+(3, 'Karyawan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id_menu` int(11) NOT NULL,
+  `menu` varchar(50) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  `urutan_menu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_menu`
+--
+
+INSERT INTO `user_menu` (`id_menu`, `menu`, `is_active`, `urutan_menu`) VALUES
+(1, 'Pengaturan', 1, 3),
+(2, 'User', 1, 1),
+(4, 'Absensi', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id_sub` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `submenu` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  `urutan_sub` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id_sub`, `id_menu`, `submenu`, `url`, `icon`, `is_active`, `urutan_sub`) VALUES
+(1, 1, 'Hak Akses', 'pengaturan/hak_akses', 'fas fa-fw fa-cogs', 1, 3),
+(2, 2, 'Dashboard', 'user', 'fas fa-fw fa-home', 1, 1),
+(3, 2, 'Data User', 'user/data_user', 'fas fa-fw fa-users', 1, 2),
+(4, 1, 'Menu', 'pengaturan/menu', 'fas fa-sliders-h', 1, 1),
+(5, 1, 'Submenu', 'pengaturan/submenu', 'fas fa-fw fa-tasks', 1, 2),
+(6, 2, 'Level', 'user/level', 'fas fa-fw fa-dot-circle', 1, 4),
+(7, 4, 'Jam Kerja', 'absensi/jam_kerja', 'fas fa-fw fa-clock', 1, 1),
+(8, 4, 'Absen Harian', 'absensi', 'fas fa-fw fa-hand-paper', 1, 2),
+(9, 4, 'Data Absensi', 'absensi/data_absensi', 'fas fa-fw fa-clipboard-list', 1, 3),
+(10, 4, 'Detail Absensi', 'absensi/detail_absensi', 'fas fa-fw fa-list', 1, 3),
+(11, 2, 'Edit Profile', 'user/edit_profile', 'fas fa-fw fa-user', 1, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`);
+
+--
+-- Indeks untuk tabel `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  ADD PRIMARY KEY (`id_jam`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `user_akses_menu`
+--
+ALTER TABLE `user_akses_menu`
+  ADD PRIMARY KEY (`id_akses`);
+
+--
+-- Indeks untuk tabel `user_level`
+--
+ALTER TABLE `user_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indeks untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indeks untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id_sub`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `jam_kerja`
+--
+ALTER TABLE `jam_kerja`
+  MODIFY `id_jam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_akses_menu`
+--
+ALTER TABLE `user_akses_menu`
+  MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_level`
+--
+ALTER TABLE `user_level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
